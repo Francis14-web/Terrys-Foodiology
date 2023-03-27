babc01e8a407906cea5a9e3ae9275d82366bd6c5
Jerico Victoria <78258443+dormammuuuuu@users.noreply.github.com>
2023-03-27 23:30:09 +0800

    Added User Messages Blade

--- resources/views/components/user/user-sidebar.blade.php
+++ resources/views/components/user/user-sidebar.blade.php
@@ -7,7 +7,7 @@
                     <x-sidebar-link route="user.dashboard" icon="bx bxs-home" label="Home" />
                     <x-sidebar-link route="user.menu" icon="bx bxs-book" label="Menu" />
                     <x-sidebar-link route="user.order" icon="bx bxs-cart-alt" label="Order" />
-                    <x-sidebar-link route="" icon="bx bxs-message" label="Messages" />
+                    <x-sidebar-link route="user.message" icon="bx bxs-message" label="Messages" />
                 </div>
             </div>
             <div class="ml-10">
