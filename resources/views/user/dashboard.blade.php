ff61c4249034e2e2498877ba832e815de1d08ee8
SiyeloIketz <124360692+SiyeloIketz@users.noreply.github.com>
2023-05-22 21:24:52 +0800

    dashboard(feat)

--- resources/views/user/dashboard.blade.php
+++ resources/views/user/dashboard.blade.php
@@ -11,7 +11,7 @@
 @section('content')
     <div class="relative w-screen h-screen">
         <div class="fixed top-0 flex items-center pl-8 h-uto w-full bg-[#1e4d2b] z-30 max-w-[762px] md:hidden">
-            <i class='bx bx-menu bx-sm'></i>  
+            <i class='bx bx-menu bx-sm' style="color:#FFFFFF"></i>  
             <img class="h-auto w-40 " src="/img/landlogo.png">          
         </div>
         <x-user.user-sidebar />
