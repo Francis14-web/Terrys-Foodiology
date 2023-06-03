39b21ecf10d7f82e90bb1f958acee0459099b868
Francis14-web <81155436+Francis14-web@users.noreply.github.com>
2023-06-04 03:11:22 +0800

    Minor Fix (FE)

--- resources/views/components/heading.blade.php
+++ resources/views/components/heading.blade.php
@@ -3,8 +3,8 @@
 ])
 
 
-<div class="hidden md:flex px-10 pt-10  items-center gap-6">
-    <div class="cursor-pointer">
+<div class="flex relative px-10 pt-20  items-center gap-6">
+    <div class="cursor-pointer sm:block hidden">
         <i class='text-3xl bx bx-x' id="menu-button"></i>
     </div>
     <p class="text-2xl font-bold font-nunito text-gray-800">{{ $title }}</p>
