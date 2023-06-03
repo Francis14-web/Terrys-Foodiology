7818cd2485baf81f3d345c5c185573e620c4272f
Francis14-web <81155436+Francis14-web@users.noreply.github.com>
2023-06-04 03:12:44 +0800

    Heading Fixed

--- resources/views/components/heading.blade.php
+++ resources/views/components/heading.blade.php
@@ -3,7 +3,7 @@
 ])
 
 
-<div class="flex relative px-10 pt-20  items-center gap-6">
+<div class="flex relative px-10 sm:pt-10 pt-20  items-center gap-6">
     <div class="cursor-pointer sm:block hidden">
         <i class='text-3xl bx bx-x' id="menu-button"></i>
     </div>
